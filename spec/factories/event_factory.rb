02cb104188c0dbcb4958c1001ad6331b72ac3985
Toan Vo <toan@tinypulse.com>
2015-12-20 21:30:44 +0700

    add view specs

--- spec/factories/event_factory.rb
+++ spec/factories/event_factory.rb
@@ -8,5 +8,11 @@ FactoryGirl.define do
     extended_html_description Faker::Lorem.paragraph(4)
     category
     sequence(:name) {|n| "Event #{n}"}
+
+    trait :with_ticket_types do
+      after :create do |event|
+        [:type_a, :type_b, :type_c].each {|type| create(:ticket_type, type, event: event)}
+      end
+    end
   end
 end
